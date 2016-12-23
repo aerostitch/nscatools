e23e0c45723e33480f1de7f116447a8adbbc1d7b
Joseph Herlant <joseph.herlant@tubemogul.com>
2016-12-22 21:59:11 -0800

    Errors should be just errored out to avoid fixing 1 error at a time

--- config_test.go
+++ config_test.go
@@ -28,22 +28,22 @@ func TestNewConfig(t *testing.T) {
 		conf := NewConfig(tt.hostIn, tt.portIn, tt.encryptionIn, tt.passwordIn, testDataHandler)
 		switch {
 		case conf.Host != tt.hostOut:
-			t.Fatalf("Unexpected hostname: %s. Expected: %s", conf.Host, tt.hostOut)
+			t.Errorf("Unexpected hostname: %s. Expected: %s", conf.Host, tt.hostOut)
 		case conf.Port != tt.portOut:
-			t.Fatalf("Unexpected port: %d. Expected: %d", conf.Port, tt.portOut)
+			t.Errorf("Unexpected port: %d. Expected: %d", conf.Port, tt.portOut)
 		case conf.EncryptionMethod != tt.encryptionOut:
-			t.Fatalf("Unexpected encryption: %d. Expected: %d", conf.EncryptionMethod, tt.encryptionOut)
+			t.Errorf("Unexpected encryption: %d. Expected: %d", conf.EncryptionMethod, tt.encryptionOut)
 		case conf.Password != tt.passwordOut:
-			t.Fatalf("Unexpected encryption: %s. Expected: %s", conf.Password, tt.passwordOut)
+			t.Errorf("Unexpected encryption: %s. Expected: %s", conf.Password, tt.passwordOut)
 		case conf.MaxHostnameSize != 64:
-			t.Fatalf("Unexpected max hostname size: %d. Expected: %d", conf.MaxHostnameSize, 64)
+			t.Errorf("Unexpected max hostname size: %d. Expected: %d", conf.MaxHostnameSize, 64)
 		case conf.MaxDescriptionSize != 128:
-			t.Fatalf("Unexpected max description size: %d. Expected: %d", conf.MaxDescriptionSize, 128)
+			t.Errorf("Unexpected max description size: %d. Expected: %d", conf.MaxDescriptionSize, 128)
 		case conf.MaxPluginOutputSize != 4096:
-			t.Fatalf("Unexpected plugin output size: %d. Expected: %d", conf.MaxPluginOutputSize, 4096)
+			t.Errorf("Unexpected plugin output size: %d. Expected: %d", conf.MaxPluginOutputSize, 4096)
 		}
 		if f := conf.PacketHandler(&DataPacket{}); f.Error() != "all good" {
-			t.Fatalf("Handler is not returning the expected value: %s", f)
+			t.Errorf("Handler is not returning the expected value: %s", f)
 		}
 	}
 
