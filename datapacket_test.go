6f46d26f8a8d50267a83162816da5ec3a4049273
Joseph Herlant <joseph.herlant@tubemogul.com>
2016-12-24 16:15:23 -0800

    Reinitializing the data packet for every function and adding Decrypt + Encrypt tests

--- datapacket_test.go
+++ datapacket_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-var newPktCases = []struct {
+type pktCases []struct {
 	versionOut      int16
 	crcOut          uint32
 	stateOut        int16
@@ -21,13 +21,30 @@ var newPktCases = []struct {
 	encryptionOut   int
 	bufferIn        []byte
 	bufferOut       []byte
-}{
-	{-1, 0, StateUnknown, "", "", "", []byte("aerosmith"), []byte("aerosmith"), []byte("freebird"), []byte("freebird"), 0, 0, []byte("breaking the law"), []byte("breaking the law")},
-	{-1, 0, StateUnknown, "", "", "", []byte("welcome to the jungle"), []byte("welcome to the jungle"), []byte("crash"), []byte("crash"), 1, 1, []byte("sympathy for the devil"), []byte{0x67, 0x6e, 0x60, 0x60, 0x66, 0x7a, 0x7f, 0x38, 0x27, 0x61, 0x2c, 0x74, 0x29, 0x62, 0x20, 0x6c, 0x27, 0x6b, 0x71, 0x72, 0x6f, 0x69}},
+}
+
+// We need to reinitialize the values every time as we change the byte arrays
+// during the testing process
+func getPktCases() *pktCases {
+	return &pktCases{
+		{
+			-1, 0, StateUnknown, "", "", "", []byte("aerosmith"), []byte("aerosmith"), []byte("freebird"), []byte("freebird"),
+			EncryptNone, EncryptNone,
+			[]byte("breaking the law"),
+			[]byte("breaking the law"),
+		},
+		{
+			-1, 0, StateUnknown, "", "", "", []byte("welcome to the jungle"), []byte("welcome to the jungle"), []byte("crash"), []byte("crash"),
+			EncryptXOR, EncryptXOR,
+			[]byte("sympathy for the devil"),
+			[]byte{0x67, 0x6e, 0x60, 0x60, 0x66, 0x7a, 0x7f, 0x38, 0x27, 0x61, 0x2c, 0x74, 0x29, 0x62, 0x20, 0x6c, 0x27, 0x6b, 0x71, 0x72, 0x6f, 0x69},
+		},
+	}
 }
 
 func TestNewDataPacket(t *testing.T) {
-	for _, tt := range newPktCases {
+	cases := getPktCases()
+	for _, tt := range *cases {
 		ts := uint32(time.Now().Unix())
 		pkt := NewDataPacket(tt.encryptionIn, tt.passwordIn, tt.ivIn)
 
@@ -66,8 +83,9 @@ func TestNewDataPacket(t *testing.T) {
 	}
 }
 
-func TestXor(t *testing.T) {
-	for _, tt := range newPktCases {
+func TestDataPacketXor(t *testing.T) {
+	cases := getPktCases()
+	for _, tt := range *cases {
 		pkt := NewDataPacket(tt.encryptionIn, tt.passwordIn, tt.ivIn)
 		pkt.xor(tt.bufferIn)
 		// When not a xor encryption, the bufferOut is not encrypted so we revert
@@ -79,4 +97,54 @@ func TestXor(t *testing.T) {
 			t.Errorf("Expecting buffer: %s, got: %s\n", tt.bufferOut, tt.bufferIn)
 		}
 	}
+}
+
+func TestDataPacketDecrypt(t *testing.T) {
+	cases := getPktCases()
+	for _, tt := range *cases {
+		pkt := NewDataPacket(tt.encryptionIn, tt.passwordIn, tt.ivIn)
+		if err := pkt.Decrypt(tt.bufferOut); err != nil {
+			if pkt.Encryption == EncryptRC6 || pkt.Encryption == EncryptMARS || pkt.Encryption == EncryptPANAMA || pkt.Encryption == EncryptIDEA {
+				if err.Error() != "Unimplemented encryption algorithm" {
+					t.Errorf("pkt.Encrypt returned: %s\n", err)
+				} else {
+					t.Logf("RC6, MARS, PANAMA and IDEA were not implemented in the NSCA original server")
+				}
+			} else {
+				t.Errorf("pkt.Decrypt returned: %s\n", err)
+			}
+		}
+		if !bytes.Equal(tt.bufferOut, tt.bufferIn) {
+			t.Errorf("Expecting buffer: %s, got: %s\n", tt.bufferIn, tt.bufferOut)
+		}
+	}
+}
+
+// Same as Encrypt but buffer in and out reverted
+func TestDataPacketEncrypt(t *testing.T) {
+	cases := getPktCases()
+	for _, tt := range *cases {
+		pkt := NewDataPacket(tt.encryptionIn, tt.passwordIn, tt.ivIn)
+		if err := pkt.Encrypt(tt.bufferIn); err != nil {
+			if pkt.Encryption == EncryptRC6 || pkt.Encryption == EncryptMARS || pkt.Encryption == EncryptPANAMA || pkt.Encryption == EncryptIDEA {
+				if err.Error() != "Unimplemented encryption algorithm" {
+					t.Errorf("pkt.Encrypt returned: %s\n", err)
+				} else {
+					t.Logf("RC6, MARS, PANAMA and IDEA were not implemented in the NSCA original server")
+				}
+			} else {
+				t.Errorf("pkt.Encrypt returned: %s\n", err)
+			}
+		}
+		if !bytes.Equal(tt.bufferIn, tt.bufferOut) {
+			t.Errorf("Expecting buffer: %s, got: %s\n", tt.bufferOut, tt.bufferIn)
+		}
+	}
+}
+
+func TestDataPacketRead(t *testing.T) {
+	t.Skip("Not implemented yet")
+}
+func TestDataPacketWrite(t *testing.T) {
+	t.Skip("Not implemented yet")
 }
